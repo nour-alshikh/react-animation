189c5fcfa5e7329f90984c98914bcf86e92f45e6
nour-alshikh <nooralshikh30@gmail.com>
2024-05-14 14:24:14 +0300

    expandaple card

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
 import { motion } from "framer-motion"
-import images from "./images"
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 function App() {
 
-  const [width, setWidth] = useState(0)
+  const [open, setIsOpen] = useState(false);
 
-  const carousel = useRef();
-
-  useEffect(() => {
-    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
-  }, [])
 
   return (
     <>
-      <div className="mx-[20%] my-[10%]">
+      <div className="w-screen h-screen bg-slate-600">
 
-        <motion.div ref={carousel} className="carousel overflow-hidden ">
-          <motion.div drag="x" dragConstraints={{ right: 0, left: -width }} className="inner_carousel flex ">
-            {images.map((image, i) => (
-              <motion.div key={i} className="item min-w-[30rem] min-h-[40rem] p-[40px]  cursor-grab">
-                <img src={image} alt="" className="w-full h-full rounded-md pointer-events-none" />
+        <div className="px-[20%] py-[10%] flex justify-center items-center">
+          <motion.div layout transition={{ layout: { duration: 1.5, type: "spring", bounce: "0.2", damping: "15" } }} onClick={() => setIsOpen(!open)} className="card bg-white p-8 rounded-lg cursor-pointer">
+            <motion.h2 layout="position" className="text-[24px] font-bold">Title</motion.h2>
+            {
+              open &&
+              <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} className="mt-4">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquam labore fugiat! Voluptates quisquam et unde architecto cumque, nobis quis.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, quaerat!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquam labore fugiat! Voluptates quisquam et unde architecto cumque, nobis quis.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, quaerat!</p>
               </motion.div>
-
-            ))}
+            }
           </motion.div>
-        </motion.div>
+        </div>
       </div>
     </>
   )
